a47883f6e57073f01e817aad22d2fd87358c06f2
Mark Stark <markstarkart@gmail.com>
2018-07-25 16:49:11 -0500

    server started and sending Hello World!

---
+++ waterGripes/server/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const port = process.env.PORT || 3000;
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Water Gripes listening on port ${port}!`));
