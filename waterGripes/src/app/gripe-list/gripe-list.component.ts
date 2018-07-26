bae0547e2b8da1443279d84068aa1748c3bd6f4d
Mark Stark <markstarkart@gmail.com>
2018-07-26 16:53:40 -0500

    gripe-list component generated

---
+++ waterGripes/src/app/gripe-list/gripe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gripe-list',
+  templateUrl: './gripe-list.component.html',
+  styleUrls: ['./gripe-list.component.css']
+})
+export class GripeListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
