0450e3e27b7bca5b11b93aad981af447d796c376
Mark Stark <markstarkart@gmail.com>
2018-07-25 23:31:12 -0500

    refactored db conections, added and connected sequlizie

--- waterGripes/server/server.js
+++ waterGripes/server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const { urlencoded, json } = require('body-parser')
+
+const db = require('../database-mysql');
+const seq= require('../database-mysql/sequelize')
+
+
 const app = express();
 
 app.use(urlencoded({ extended: false }))
