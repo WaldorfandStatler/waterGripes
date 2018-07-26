93bf43b084e605ddf6a1fffb265bb8e45925f27d
Mark Stark <markstarkart@gmail.com>
2018-07-26 17:32:49 -0500

    building out gripe-list component

--- waterGripes/src/app/gripe-list/gripe-list.component.ts
+++ waterGripes/src/app/gripe-list/gripe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gripe } from '../gripe';
 
 @Component({
   selector: 'app-gripe-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gripe-list.component.css']
 })
 export class GripeListComponent implements OnInit {
-
+  gripe: Gripe = {
+    id: 1,
+    block_number: 800,
+    street: 'St Charles',
+    crossStreet: 'Washington',
+    zipcode: 70115,
+    latitude: null,
+    longitude: null,
+    gripe: 'sewer leak',
+    comment: '',
+    votes: 5,
+    user_id: 1,
+    status: 'Unresolved'
+  }
   constructor() { }
 
   ngOnInit() {
---
+++ waterGripes/src/app/gripe.ts
@@ -0,0 +1,14 @@
+export class Gripe {
+  id: number;
+  block_number: number;
+  street: string;
+  crossStreet: string;
+  zipcode: number;
+  latitude: number;
+  longitude: number;
+  gripe: string;
+  comment: string;
+  votes: number;
+  user_id: number;
+  status: string;
+}
