6615538fe5f269cda55f6343e1730cff2e938baa
josef butts <buttsjosef@gmail.com>
2018-07-26 07:25:04 -0500

    fixed config and index to deploy on digital ocean

--- waterGripes/database-mysql/config.js
+++ waterGripes/database-mysql/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'password',
   database: 'watergripes'
 };
--- waterGripes/database-mysql/index.js
+++ waterGripes/database-mysql/index.js
@@ -4,15 +4,9 @@ const connection = mysql.createConnection(mysqlConfig);
 
 
 connection.connect( (err)=>{
-<<<<<<< HEAD
-  if (err) {
-    console.error(err);
-    return;
-=======
   if (err) { 
     console.error(err); 
     return;  
->>>>>>> 4a6aea91023d599ae7e986ae01a32a91e6786ed5
   }
   console.log( "connected to database" );
 });
