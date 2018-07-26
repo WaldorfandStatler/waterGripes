4223a56e993ebba23851d6135ec5ca28b5d62673
josef butts <buttsjosef@gmail.com>
2018-07-26 16:48:45 -0500

    updated schema

---
+++ waterGripes/database-mysql/sequelize/helpers.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+
+
+const getAllGripes = ()=>{};
+
+const getLocationData = (id)=>{};
+
+const submitGripe = (gripe)=>{};
+
+const updateGripe = (id, gripeVote)=>{};
+
+const getGripeLocationById = (id) =>{};
+
+const createUser = (userInfo)=>{};
+
+const loginUser = (user) => {};
+
+
+ 
+
+
--- waterGripes/schema.sql
+++ waterGripes/schema.sql
@@ -17,6 +17,7 @@ CREATE TABLE gripes (
   id INTEGER NOT NULL AUTO_INCREMENT,
   block_number INTEGER NOT NULL,
   street VARCHAR(100) NOT NULL,
+  cross_street VARCHAR(100) NOT NULL,
   zipcode INTEGER NOT NULL,
   latitude DECIMAL(11,8),
   longitude DECIMAL(11,8),
@@ -39,9 +40,9 @@ INSERT into users (username, email, zipcode, PASSWORD) VALUES ("buddy1", "buddy1
 INSERT into users (username, email, zipcode, PASSWORD) VALUES ("buddy2", "buddy2@gmail.com", "70118", "12345"); 
 INSERT into users (username, email, zipcode, PASSWORD) VALUES ("buddy", "buddy3@gmail.com", "70117", 'password'); 
 
-INSERT into gripes (block_number, street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
-  VALUES (200, "St. Charles", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
-INSERT into gripes (block_number, street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
-  VALUES (201, "St. Charles", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
-INSERT into gripes (block_number, street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
-  VALUES (202, "St. Charles", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
+INSERT into gripes (block_number, street, cross_street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
+  VALUES (200, "St. Charles", "3rd", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
+INSERT into gripes (block_number, street, cross_street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
+  VALUES (201, "St. Charles", "3rd", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
+INSERT into gripes (block_number, street, cross_street, zipcode, latitude, longitude, gripe, comment, resolvedVotes, unresolvedVotes, status) 
+  VALUES (202, "St. Charles", "3rd", "70130", 30, 90. "water leaking from fire hydrant", "lots of water", 0, 0); 
