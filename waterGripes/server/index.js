96ab3a98f9892166328635a2c2b7beefcd7e6efb
josef butts <buttsjosef@gmail.com>
2018-07-26 07:26:44 -0500

    changed port for digital ocean

--- waterGripes/server/index.js
+++ waterGripes/server/index.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 3000;
+const port =  80 || process.env.PORT || 3000;
 const app = require('./server');
   
 app.listen(port, () => console.log(`Water Gripes listening on port ${port}!`));
