d8bd7c6bf4fd0ae80424c2822ae98977b7349f74
josef butts <buttsjosef@gmail.com>
2018-07-25 20:06:28 -0500

    created schema

--- waterGripes/schema.sql
+++ waterGripes/schema.sql
@@ -1,39 +1,39 @@
-DROP DATABASE IF EXISTS waterGripes;
+DROP DATABASE IF EXISTS watergripes;
 
-CREATE DATABASE waterGripes;
+CREATE DATABASE watergripes;
 
-USE waterGripes;
+USE watergripes;
 
 CREATE TABLE users (
-  user_id int NOT NULL AUTO_INCREMENT,
-  user_name varchar(255) NOT NULL,
-  email varchar(255) NOT NULL,
-  zipcode integer NOT NULL,
-  PRIMARY KEY (user_id)
+  id INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  zipcode INTEGER NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE gripes (
-  id int NOT NULL AUTO_INCREMENT,
-  block_number integer NOT NULL,
-  street varchar(255) NOT NULL,
-  zipcode integer NOT NULL,
-  lat DECIMAL(11 ,8),
-  long DECIMAL(11, 8),
-  gripe varchar(255) NOT NULL,
-  comment varchar(255) NOT NULL,
-  resolvedVotes integer NOT NULL,
-  unresolvedVotes integer NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  status ENUM('Resolved', 'Unresolved') NOT NULL,
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  block_number INTEGER NOT NULL,
+  street VARCHAR(100) NOT NULL,
+  zipcode INTEGER NOT NULL,
+  latitude DECIMAL(11,8),
+  longitude DECIMAL(11,8),
+  gripe VARCHAR(255) NOT NULL,
+  comment VARCHAR(255) NOT NULL,
+  resolvedVotes INTEGER NOT NULL,
+  unresolvedVotes INTEGER NOT NULL,
+  FOREIGN KEY (id) REFERENCES users(id),
+  status ENUM('Resolved', 'Unresolved') NOT NULL
 );
 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u <USER> < schema.sql
- *    OR
- *    mysql -u <USER> -p < schema.sql
+-- /*  Execute this file from the command line by typing:
+--  *    mysql -u <USER> < schema.sql
+--  *    OR
+--  *    mysql -u <USER> -p < schema.sql
  
-*/
+-- */
 
 
