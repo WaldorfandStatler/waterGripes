6211ad0c85eb6464ce7e2033a268d584e1fa4be1
Mark Stark <markstarkart@gmail.com>
2018-07-26 15:41:46 -0500

    added create user end point

--- waterGripes/server/server.js
+++ waterGripes/server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { urlencoded, json } = require('body-parser')
 
 const db = require('../database-mysql/sequelize')
-
+// console.log(db);
 
 const app = express();
 
@@ -12,6 +12,8 @@ app.use(json())
 
 
 app.use(express.static(path.resolve(__dirname, '../dist/waterGripes')));
+
+///////////gripe endpoints///////////////
 
 //endpoint to retrive all existing gripes
 app.get('/gripes', (req, res) => {
@@ -26,15 +28,15 @@ app.get('/gripes', (req, res) => {
   res.send('here are the Gripes');
 });
 
-//get get location
-app.get('/getLocation', (req, res) => {
+//get get location might handle in browser
+// app.get('/getLocation', (req, res) => {
 
-  //grab geolaction data
-  // from data get lat , long, block #, street, cross street, zip code
-  // add send back to client to be put in post req for gripes
-  // console.log(req.connection.remoteAddress.slice(7));
-  res.send('getting location');
-})
+//   //grab geolaction data
+//   // from data get lat , long, block #, street, cross street, zip code
+//   // add send back to client to be put in post req for gripes
+//   // console.log(req.connection.remoteAddress.slice(7));
+//   res.send('getting location');
+// })
 //add a gripe to db.
 app.post('/gripes', (req, res)=>{
   const gripe = req.body;
@@ -67,7 +69,6 @@ app.patch(`/gripes/:id`, (req, res) => {
 app.get('/gripe/:id/map', (req, res) => {
   const gripeId = req.headers.id;
   console.log(gripeId);
-  
   //write db get gripe by id func
   // db.getGripeLocationbyId(gripeId)
   //   .then(gripe => {
@@ -80,6 +81,20 @@ app.get('/gripe/:id/map', (req, res) => {
   //   .catch(err => console.error(err));
   res.send('getting map')
 });
+
+///user endpoints//////////
+app.post('/createUser', (req, res) => {
+  const user = req.body;
+  console.log(user);
+  // db.createUser(user)
+  //   .then(user => {
+  //     console.log(user, 'added to system');
+  //     res.send(`${user.userName} welcomed to WaterGripes`);
+  //   })
+  //   .catch(err => console.error(err));
+  res.send('user added to WaterGripes');
+});
+
 
 module.exports = app;
 //comments
