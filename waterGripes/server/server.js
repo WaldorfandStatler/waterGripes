8f9a8a94e4c196b05e9fcfd73e3631c283147fdf
josef butts <buttsjosef@gmail.com>
2018-07-26 15:35:20 -0500

    ready to merge

--- waterGripes/server/server.js
+++ waterGripes/server/server.js
@@ -14,6 +14,38 @@ app.use(json())
 
 app.use(express.static(path.resolve(__dirname, '../dist/waterGripes')));
 
+//endpoint to retrieva all existing gripes
+app.get('/gripes', (req, res) => {
+  db.getAllGripes()
+    .then((response) => {
+      console.log('get all gripes server response');
+      res.send(response);
+    })
+    .catch((err) => {
+      console.error("error retrieving database data");
+    });
+});
+
+app.post('/gripes', (req, res)=>{
+  console.log('app.post /gripes req: ', req);
+  const gripe = req.headers;
+  db.submitGripe(gripe)
+    .then((response) => {
+      console.log('submit gripes server response');
+      res.send(response);
+    })
+    .catch((err) => {
+      console.error("error posting gripe", err);
+    });
+})
+
+
+app.patch(`/gripes/:id`, (req, res) => {
+  console.log( 'patch server', req.headers.gripeId, req.headers.vote);
+  const gripeId = req.headers.GripeId;
+  const status = req.headers.vote;
+  db.updateGripe(gripeId, status);
+    });
 
 
 module.exports = app;
