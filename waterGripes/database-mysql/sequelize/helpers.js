db94b1d69f14895dda1236010537e1618aec1860
josef butts <buttsjosef@gmail.com>
2018-07-26 21:14:47 -0500

    refactoring data for Sequelize. If I'd only known

--- waterGripes/database-mysql/sequelize/helpers.js
+++ waterGripes/database-mysql/sequelize/helpers.js
@@ -12,7 +12,24 @@ const updateGripe = (id, gripeVote)=>{};
 
 const getGripeLocationById = (id) =>{};
 
-const createUser = (userInfo)=>{};
+const createUser = (userInfo)=>{
+  let name = userInfo.name;
+  let zipcode = userInfo.zipcode;
+  let email = userInfo.email;
+  let password = userinfo.password;
+
+  
+
+
+  // force: true will drop the table if it already exists
+//   User.sync({force: true}).then(() => {
+//     // Table created
+//     return User.create({
+//       firstName: 'John',
+//       lastName: 'Hancock'
+//     });
+//   });
+// };
 
 const loginUser = (user) => {};
 
