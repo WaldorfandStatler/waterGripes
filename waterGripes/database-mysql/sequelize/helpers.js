78c460d110ab4b305af16b23e355dc8fc558d39b
josef butts <buttsjosef@gmail.com>
2018-07-26 22:16:40 -0500

    fxing more merge problems

--- waterGripes/database-mysql/sequelize/helpers.js
+++ waterGripes/database-mysql/sequelize/helpers.js
@@ -18,22 +18,6 @@ const createUser = (userInfo)=>{
   let email = userInfo.email;
   let password = userinfo.password;
 
-<<<<<<< HEAD
-//  force: true will drop the table if it already exists
-  User.sync({force: true}).then(() => {
-    // Table created
-    return User.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
-};
-
-
-}
-
-=======
->>>>>>> 8aae5bbe989bbe0b5d6f1bf06f16b08158ec8b2d
   
 
 
