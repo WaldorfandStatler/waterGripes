f1a4071b9aa72fc1c8c6f7bcc040d64488e26409
Mark Stark <markstarkart@gmail.com>
2018-07-26 21:03:31 -0500

    added routes

--- waterGripes/src/app/app-routing.module.ts
+++ waterGripes/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AddGripe } from './add-gripe/add-gripe.component'
+import { CreateUser } from './create-user/create-user.component';
+import { GripesList } from './gripe-list/gripe-list.component';
+import { GripesListEntry } from './gripe-list-entry/gripe-list-entry.component';
+import { Login } from './login/login.component';
+import { UserHome } from './user-home/user-home.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/gripesList', pathMatch: 'full' },
+  { path: 'addGripe', component: AddGripeComponent },
+  { path: 'createUser', component: CreateUserComponent },
+  { path: 'gripesList', component: GripesListComponent },
+  { path: 'gripes/:id', component: GripesListEntryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'userHome', component: UserHomeComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
