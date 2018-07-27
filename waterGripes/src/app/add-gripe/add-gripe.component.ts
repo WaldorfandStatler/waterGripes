7c32af2c2ecc1bf89beca0ff9a11075a171de535
josef butts <buttsjosef@gmail.com>
2018-07-27 18:14:08 -0500

    merger changes

---
+++ waterGripes/src/app/add-gripe/add-gripe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-gripe',
+  templateUrl: './add-gripe.component.html',
+  styleUrls: ['./add-gripe.component.css']
+})
+export class AddGripeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
