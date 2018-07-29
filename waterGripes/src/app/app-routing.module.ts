35f7437cf4a734777d3f566d4e1d1505b7c35215
Mark Stark <markstarkart@gmail.com>
2018-07-29 01:05:12 -0500

    clean up

--- waterGripes/src/app/app-routing.module.ts
+++ waterGripes/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 
 import { AddGripeComponent } from './add-gripe/add-gripe.component';
 import { GripeListComponent } from './gripe-list/gripe-list.component';
@@ -7,16 +7,15 @@ import { GripeListEntryComponent } from './gripe-list-entry/gripe-list-entry.com
 import { LoginComponent } from './login/login.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 
-
 const routes: Routes = [
-  { path: '', redirectTo: '/userHome', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'gripesList', component: GripeListComponent },
   { path: 'addGripe', component: AddGripeComponent },//need to be logged in
   { path: 'gripe/:id', component: GripeListEntryComponent },//need to be logged in
   { path: 'userHome', component: UserHomeComponent },//need to be logged in
 ];
-
+// canActivate: [sessionStorage.user] investigate more
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
